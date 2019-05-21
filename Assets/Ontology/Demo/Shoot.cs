d7f3458a448923edbe91fd1dbb4d4114a73d7313
xizho10 <josh_chow@163.com>
2019-05-21 21:30:07 +0800

    use UnityWebRequest

--- Assets/Ontology/Demo/Shoot.cs
+++ Assets/Ontology/Demo/Shoot.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using OntologyCSharpSDK;
 using OntologyCSharpSDK.Network;
+using OntologyCSharpSDK.Core;
 using OntologyCSharpSDK.Interface;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-public class Shoot : MonoBehaviour {
+//https://blog.csdn.net/qwe25878/article/details/85051911
+public class Shoot : MonoBehaviour
+{
     public GameObject bullet;
     public GameObject text;
     public float speed = 5;
@@ -14,34 +20,60 @@ public class Shoot : MonoBehaviour {
 
     static OntologySdk OntSDK = new OntologySdk(_node, ConnectionMethodFactory.ConnectionMethod.REST);
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         InvokeRepeating("Tick", 0, 20f);
     }
     void Tick()
     {
-        try
+        StartCoroutine(Get());
+
+    }
+    IEnumerator Get()
+    {
+
+        /* 
+          long height = OntSDK.Connection.getBlockHeight();
+           Debug.Log("wait 20s,Get Block Height: " + height);
+        */
+
+        /*   follow this demo: https://github.com/ontio-community/ontology-csharp-sdk
+         *  Transaction tx = TransactionBuilder.MakeInvokeTransaction(action);
+         *   OntSDK.sign(tx, privatekeys);
+         *  var jsonObject = new JObject
+         *   {
+         *           ["Action"] = "sendrawtransaction",
+         *           ["Version"] = "1.0.0",
+         *           ["Data"] = tx.toHexString()
+         *   };
+         *   UnityWebRequest.Post(Constants.REST_sendRawTransactionPreExec, JsonConvert.SerializeObject(jsonObject));         
+         *
+         */
+
+        UnityWebRequest webRequest = UnityWebRequest.Get(_node + Constants.REST_getBlockHeight);
+        yield return webRequest.SendWebRequest();
+        if (webRequest.isHttpError || webRequest.isNetworkError)
         {
-              long height = OntSDK.Connection.getBlockHeight();
-               Debug.Log("wait 20s,Get Block Height: " + height);
-
-               TextMesh tt = GameObject.Find("Text").GetComponent<TextMesh>();
-               tt.text = "Ontology Block Height: " + height.ToString();
-
+            Debug.Log(webRequest.error);
         }
-        catch
+        else
         {
+            Debug.Log("response:" + webRequest.downloadHandler.text);
+        }
+        TextMesh tt = GameObject.Find("Text").GetComponent<TextMesh>();
 
-        };
+        tt.text = "Ontology Block Height: " + JObject.Parse(webRequest.downloadHandler.text)["Result"].ToString();
+
     }
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject b = GameObject.Instantiate(bullet,transform.position,transform.rotation);
+            GameObject b = GameObject.Instantiate(bullet, transform.position, transform.rotation);
             Rigidbody rgd = b.GetComponent<Rigidbody>();
             rgd.velocity = transform.forward * speed;
 
-        }      
-
+        }
     }
 }
